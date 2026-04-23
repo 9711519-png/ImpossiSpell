916a05a732f4f8cd17498c0357f94f0904558bfc
9711519-png <9711519@graniteschools.org>
2026-04-23 12:29:26 -0600

    Update and rename ScoreManager to ScoreManager.java

---
+++ src/ScoreManager.java
@@ -0,0 +1,16 @@
+public class ScoreManager {
+
+  private int score;
+
+  public ScoreManager() {
+      this.score = 0;
+  }
+
+  public void addPoints(int multiplier) {
+      // add points based on difficulty
+  }
+
+  public int getScore() {
+      return score:
+  }
+}
