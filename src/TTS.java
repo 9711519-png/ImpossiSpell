729410c803439b147c4fa5715756f1acec426a73
9711519-png <9711519@graniteschools.org>
2026-04-23 12:32:50 -0600

    Add TTS class with basic methods

--- src/TTS.java
+++ src/TTS.java
@@ -1,1 +1,14 @@
+public class TTS {
 
+  public TTS() {
+    // setup TTS engine
+  }
+
+  public void speak(String word) {
+    //speak word
+  }
+
+  public void repeat() {
+    // repeat ;ast word
+  }
+}
