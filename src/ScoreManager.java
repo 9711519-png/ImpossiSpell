4c5a747e0f9427bf29592a1644bf1761c93504cd
9711519-png <9711519@graniteschools.org>
2026-05-01 22:40:31 -0600

    Update ScoreManager.java

--- src/ScoreManager.java
+++ src/ScoreManager.java
@@ -1,16 +1,17 @@
 public class ScoreManager {
 
-  private int score;
+  private int score; // player's total score
 
   public ScoreManager() {
-    this.score = 0;
+    this.score = 0; // start at 0
   }
 
   public void addPoints(int multiplier) {
+    // add points based on difficulty
     score += 10 * multiplier;
   }
 
   public int getScore() {
-    return score;
+    return score; // return current score
   }
 }
