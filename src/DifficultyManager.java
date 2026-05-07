0498b626e50d646478a53abbdee2c3cd19575c66
9711519-png <9711519@graniteschools.org>
2026-05-06 23:00:57 -0600

    Update DifficultyManager.java

--- src/DifficultyManager.java
+++ src/DifficultyManager.java
@@ -1,16 +1,25 @@
 public class DifficultyManager {
 
-    private int level = 0;
+    private int level = 0; // current difficulty level
 
+    // difficulty names in order
     private String[] levels = {
-        "easy", "medium", "hard", "difficult", "expert", "insane", "impossible"
+        "easy", "medium", "hard", "difficult",
+        "expert", "insane", "impossible"
     };
 
     public String getCurrentDifficulty() {
+        // return current difficulty name
         return levels[level];
     }
 
+    public int getDifficultyMultiplier() {
+        // score multiplier based on level
+        return level + 1;
+    }
+
     public void increaseDifficulty() {
+        // move to next difficulty if possible
         if (level < levels.length - 1) {
             level++;
         }
